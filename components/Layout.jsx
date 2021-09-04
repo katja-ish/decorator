46180987d9595b51aab80eea97f7616e004b3d06
semilunar <katyanikitina.me@yandex.ru>
2021-09-04 21:26:45 +0300

    fix: transition duratuon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,9 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { NextSeo } from "next-seo";
 import { motion } from "framer-motion";
 
 const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
+  hidden: { opacity: 0, x: -100, y: 0 },
   enter: { opacity: 1, x: 0, y: 0 },
   exit: { opacity: 0, x: 0, y: -100 },
 };
@@ -20,7 +20,7 @@ const Layout = ({ children, title, description }) => (
       animate="enter"
       exit="exit"
       variants={variants}
-      transition={{ type: "linear", duration: 0.3 }}
+      transition={{ type: "linear", duration: 0.75 }}
     >
       {children}
     </motion.main>
